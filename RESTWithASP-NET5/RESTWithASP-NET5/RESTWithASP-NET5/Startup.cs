21a361e52502aabc49c26bbc177875bc16a47200
Filipe Chaves <filipechavs7@gmail.com>
2021-07-28 15:01:23 -0300

    docker metade

--- RESTWithASP-NET5/RESTWithASP-NET5/RESTWithASP-NET5/Startup.cs
+++ RESTWithASP-NET5/RESTWithASP-NET5/RESTWithASP-NET5/Startup.cs
@@ -97,11 +97,10 @@ namespace RESTWithASP_NET5
             var connection = Configuration["MySqlConnection:MySqlConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, serverVersion));
 
-            if (Enviroment.IsDevelopment())
+            /*if (Enviroment.IsDevelopment())
             {
                 MigrateDatabase(connection);
-            }
-
+            }*/
             services.AddMvc(options =>
             {
                 options.RespectBrowserAcceptHeader = true;
